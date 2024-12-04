219d168d1c7abacd574a46bcce871bc863779379
Sufi <120452154+MarcalekM@users.noreply.github.com>
2024-12-04 18:30:51 +0100

    QuickFix

--- Assets/Scripts/EndGameBackToMenu.cs
+++ Assets/Scripts/EndGameBackToMenu.cs
@@ -7,8 +7,9 @@ public class EndGameBackToMenu : MonoBehaviour
 {
     private void Awake()
     {
+       if(GameData.sanity < 10) GameData.sanity = 10;
+        if (GameData.Timer < 1800) GameData.Timer = 1800;
         StartCoroutine(LoadLevel("Menu").GetEnumerator());
-
     }
     IEnumerable LoadLevel(string sceneName)
     {
